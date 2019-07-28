412b7ba6e7f517c4131c27933b6021f1f8f8af3b
Jacquelynne Heiman <jacheima@uat.edu>
2019-07-28 12:49:44 -0700

    Scriptable Objects
    I got the scriptable objects all set up for items and events. An NPC entering an item trigger causes the event to be called. I have listeners in the scene that respond to the events. Pretty cool! I am working on implementing it with the NPCs that Will set up yesterday.

---
+++ BossSimulator/Assets/Needs.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needs : MonoBehaviour
+{
+    public int Happiness = 0;
+    public int Productivity = 0;
+    public int Social = 0;
+}
