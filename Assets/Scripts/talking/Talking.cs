41d3e24c051c8f7024b7ad053115b6c286cb6138
Tucbaski <tucbaski@uat.edu>
2019-09-19 17:25:11 -0700

    fixed some thing got everything worked

--- Assets/Scripts/talking/Talking.cs
+++ Assets/Scripts/talking/Talking.cs
@@ -8,14 +8,19 @@ public class Talking : MonoBehaviour
     private List<chat> Emotion = new List<chat>();
     public Text emotionText;
     public float delayBeforeNext;
-
-
+    public Text AItext;
+    public Text boss_Answer;
+    public Text boss_Answer2;
+    public Text boss_Answer3;
+    public Text boss_Answer4;
+    private List<choosingxaxis> lockanswer = new List<choosingxaxis>();
+    public string sata;
     // Start is called before the first frame update
     void Start()
     {
         TextAsset Emotiondata = Resources.Load<TextAsset>("EmotionPlaytest");
         string[] data = Emotiondata.text.Split(new char[] {'\n'});
-
+        
         for (int i = 1; i < data.Length - 1; i++)
         {
             string[] row = data[i].Split(new char[ ]{','});
@@ -33,6 +38,30 @@ public class Talking : MonoBehaviour
             }
         }
 
+        TextAsset LockonData = Resources.Load<TextAsset>("EmotionPlaytest");
+        string[] beta = LockonData.text.Split(new char[] { ',' });
+
+        for (int i = 1; i < beta.Length - 1; i++)
+        {
+            string[] Xx = beta[i].Split(new char[] { '\n' });
+            if (Xx[1] != "")
+            {
+                choosingxaxis T = new choosingxaxis();
+                int.TryParse(Xx[0], out T.IDX);
+                T.X1 = Xx[1];
+                T.X2 = Xx[2];
+                T.X3 = Xx[3];
+                T.X4 = Xx[4];
+                T.X5 = Xx[5];
+
+
+
+            }
+        }
+
+
+
+
         //foreach (chat q in Emotion)
         //{
         //    Debug.Log(q.C1 + "," + q.C3);
@@ -45,7 +74,13 @@ public class Talking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetMouseButtonDown(0))
+        {
+            foreach (chat q in Emotion)
+            {
+                boss_Answer.text = q.C2;
+            }
+       }
     }
 
     IEnumerator waity()
@@ -53,7 +88,7 @@ public class Talking : MonoBehaviour
         foreach (chat q in Emotion)
         {
             emotionText.text = q.C1 + "," + q.C3;
-            yield return new WaitForSeconds(delayBeforeNext);
+            yield return new WaitForSeconds();
         }
     }
 }
---
+++ Assets/Scripts/talking/choosing x axis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class choosingxaxis
+{
+
+
+    public int IDX;
+    public string X1;
+    public string X2;
+    public string X3;
+    public string X4;
+    public string X5;
+    public string X6;
+    public string X7;
+    public string X8;
+    public string X9;
+    public string X10;
+    public string X11;
+
+
+
+
+}
